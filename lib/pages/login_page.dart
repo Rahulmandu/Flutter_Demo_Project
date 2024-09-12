f49b037896d2e0a1d40b464a0473935d3d876e35
Rahul <rahul.mondal@gyansys.com>
2024-09-12 16:25:49 +0530

    Changes to pubspec.yaml , Login Page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,11 +6,78 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text("Login Form",
+    return Material(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/login_image.png',
+            fit: BoxFit.contain,
+            width: double
+                .infinity, // To make the image take the full width of the screen
+            height: 300, // You can adjust this height as needed
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Text(
+            "Welcome",
             style: TextStyle(
-                fontSize: 40, color: Colors.blue, fontWeight: FontWeight.bold)),
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter User Name",
+                        labelText: "User Name",
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Add your logic here
+                        if (kDebugMode) {
+                          print("Login Successful");
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15), // Adds padding to the button
+                        backgroundColor: Colors.deepPurple, // Background color
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30), // Rounded corners
+                        ),
+                      ),
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 18, // Font size
+                          color: Colors.white, // Text color
+                          fontWeight: FontWeight.bold, // Bold text
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
