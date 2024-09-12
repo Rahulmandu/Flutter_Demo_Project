4264bb73071d7488c38698a47d461cc72ebc7247
Rahul <rahul.mondal@gyansys.com>
2024-09-12 14:06:36 +0530

    Changes to Home page

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String author_name = "Rahul Mondal";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $author_name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
